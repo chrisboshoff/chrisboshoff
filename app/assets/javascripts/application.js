e9c532454e7bb99d3fcdf45f6751717d739066c3
unknown <chris@Chris-IBM.mymobility.local>
2014-04-11 09:00:03 +0200

    move js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,3 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap.min
-
-function isMobile()
-{
-    var agent = navigator.userAgent.toLowerCase();
-    if (agent.indexOf('iphone') !== -1 ||
-            agent.indexOf('ipad') !== -1 ||
-            agent.indexOf('ipod') !== -1 ||
-            agent.indexOf('android') !== -1 ||
-            agent.indexOf('webos') !== -1 ||
-            agent.indexOf('mobile') !== -1)
-        return true;
-    else
-        return false;
-}
---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,15 @@
+function isMobile()
+{
+    var agent = navigator.userAgent.toLowerCase();
+    if (agent.indexOf('iphone') !== -1 ||
+            agent.indexOf('ipad') !== -1 ||
+            agent.indexOf('ipod') !== -1 ||
+            agent.indexOf('android') !== -1 ||
+            agent.indexOf('webos') !== -1 ||
+            agent.indexOf('mobile') !== -1)
+        return true;
+    else
+        return false;
+}
+
+
